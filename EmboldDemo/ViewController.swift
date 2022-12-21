f87ae2353d686fc40cc038bc6a060197ee1a8e26
Pare <p.pimpapare@gmail.com>
2022-12-22 00:28:29 +0700

    init commit

--- EmboldDemo/ViewController.swift
+++ EmboldDemo/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var number: Int = 0
+        number = 3
+        
+        let text: String!
+        text = nil
     }
-
-
 }
 
