32180492f1e4873fe8c212b7e596ed29b82ed304
Pare <p.pimpapare@gmail.com>
2022-12-22 00:26:53 +0700

    Initial Commit

---
+++ EmboldDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmboldDemo
+//
+//  Created by Foodstory on 22/12/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
